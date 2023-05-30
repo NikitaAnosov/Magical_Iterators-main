e938914e38189061c0a7ddb97f09e6a7396a1e1f
NikitaAnosov <anosov.nikita@gmail.com>
2023-05-30 13:01:17 +0300

    PrimeIterator

--- sources/MagicalContainer.cpp
+++ sources/MagicalContainer.cpp
@@ -1,41 +1,52 @@
 #include "MagicalContainer.hpp"
 using namespace ariel;
 
-void MagicalContainer::addElement(int element) {
+void MagicalContainer::addElement(int element)
+{
     elements.push_back(element);
 }
 
-void MagicalContainer::removeElement(int element) {
-    for (auto it = elements.begin(); it != elements.end();) {
-        if (*it == element) {
+void MagicalContainer::removeElement(int element)
+{
+    for (auto it = elements.begin(); it != elements.end();)
+    {
+        if (*it == element)
+        {
             it = elements.erase(it);
         }
-        else {
+        else
+        {
             ++it;
         }
     }
 }
 
-int MagicalContainer::size() const {
+int MagicalContainer::size() const
+{
     return elements.size();
 }
 
-MagicalContainer::AscendingIterator::AscendingIterator(const MagicalContainer& container)
-    : container(container) {
+MagicalContainer::AscendingIterator::AscendingIterator(const MagicalContainer &container)
+    : container(container)
+{
     sortedElements = container.elements;
+    // using std::sort library function
     std::sort(sortedElements.begin(), sortedElements.end());
 }
 
-vector<int>::iterator MagicalContainer::AscendingIterator::begin() {
+vector<int>::iterator MagicalContainer::AscendingIterator::begin()
+{
     return sortedElements.begin();
 }
 
-vector<int>::iterator MagicalContainer::AscendingIterator::end(){
+vector<int>::iterator MagicalContainer::AscendingIterator::end()
+{
     return sortedElements.end();
 }
 
-MagicalContainer::SideCrossIterator::SideCrossIterator(const MagicalContainer& container)
-    : container(container) {
+MagicalContainer::SideCrossIterator::SideCrossIterator(const MagicalContainer &container)
+    : container(container)
+{
     AscendingIterator ascIter(container);
     DescendingIterator descIter(container);
 
@@ -44,7 +55,8 @@ MagicalContainer::SideCrossIterator::SideCrossIterator(const MagicalContainer& c
     int count = 0;
     int maxSize = container.size() / 2;
 
-    while (ascIt != ascIter.end() && descIt != descIter.end() && count < maxSize) {
+    while (ascIt != ascIter.end() && descIt != descIter.end() && count < maxSize)
+    {
         crossElements.push_back(*ascIt);
         ++ascIt;
         crossElements.push_back(*descIt);
@@ -53,54 +65,71 @@ MagicalContainer::SideCrossIterator::SideCrossIterator(const MagicalContainer& c
     }
 }
 
-std::vector<int>::iterator MagicalContainer::SideCrossIterator::begin() {
+std::vector<int>::iterator MagicalContainer::SideCrossIterator::begin()
+{
     return crossElements.begin();
 }
 
-std::vector<int>::iterator MagicalContainer::SideCrossIterator::end() {
+std::vector<int>::iterator MagicalContainer::SideCrossIterator::end()
+{
     return crossElements.end();
 }
 
-
-
-MagicalContainer::PrimeIterator::PrimeIterator(const MagicalContainer& container)
-    : container(container) {
-    for (int element : container.elements) {
+MagicalContainer::PrimeIterator::PrimeIterator(const MagicalContainer &container)
+    : container(container)
+{
+    for (int element : container.elements)
+    {
         bool isPrime = true;
-        if (element < 2) {
+        if (element == 2)
+        {
+            isPrime = true;
+        }
+        else if (element % 2 == 0)
+        {
             isPrime = false;
-        } else {
-            for (int i = 2; i * i <= element; ++i) {
-                if (element % i == 0) {
-                    isPrime = false;
-                    break;
-                }
+        }
+        else{
+        for (int i = 3; i * i <= element; i += 2)
+        {
+            if (element % i == 0)
+            {
+                isPrime = false;
+                break;
             }
         }
-        if (isPrime) {
+        }
+        if (isPrime)
+        {
             primeElements.push_back(element);
         }
     }
+    std::sort(primeElements.begin(), primeElements.end());
 }
 
-vector<int>::iterator MagicalContainer::PrimeIterator::begin() {
+vector<int>::iterator MagicalContainer::PrimeIterator::begin()
+{
     return primeElements.begin();
 }
 
-vector<int>::iterator MagicalContainer::PrimeIterator::end() {
+vector<int>::iterator MagicalContainer::PrimeIterator::end()
+{
     return primeElements.end();
 }
 
-MagicalContainer::DescendingIterator::DescendingIterator(const MagicalContainer& container)
-    : container(container) {
+MagicalContainer::DescendingIterator::DescendingIterator(const MagicalContainer &container)
+    : container(container)
+{
     sortedElements = container.elements;
     std::sort(sortedElements.rbegin(), sortedElements.rend());
 }
 
-std::vector<int>::iterator MagicalContainer::DescendingIterator::begin() {
+std::vector<int>::iterator MagicalContainer::DescendingIterator::begin()
+{
     return sortedElements.begin();
 }
 
-std::vector<int>::iterator MagicalContainer::DescendingIterator::end() {
+std::vector<int>::iterator MagicalContainer::DescendingIterator::end()
+{
     return sortedElements.end();
 }
